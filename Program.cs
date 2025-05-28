b4e7b4fc4084c54f10fc1703f87cc4cd1a24e593
subin1848 <s2315@e-mirim.hs.kr>
2025-05-28 10:10:45 +0900

    오버로딩 주의점

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace PropExample
     {
         static void Main(string[] args)
         {
+            TestOver(3);
         }
+
+        // 오버로딩 주의점
+        //public static int TestOver(int input) { return 0; }
+        // int 타입 TestOver 메서드를 지워도 자동형변환이 되므로 double 타입의 TestOver 메서드로 간다.
+        public static bool TestOver(double input) { return true; }
     }
 }
