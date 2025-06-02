54b3b7af6c8f1ecd1f869d28a88deb2605d2f605
subin1848 <s2315@e-mirim.hs.kr>
2025-06-02 13:54:31 +0900

    정적 생성자 예제1

--- Program.cs
+++ Program.cs
@@ -16,6 +16,15 @@ namespace PropExample
             //Product p1 = new Product("사과", 2000);
             // 팩토리 메서드 패턴 - private 생성자 사용
             Product p = Product.getInstance("바나나", 2000);
+            
+            // 정적 생성자 예제1
+            Console.WriteLine("첫 번째 위치");
+            Console.WriteLine(Sample.value);    //Sample.value 값 조회 전에 정적 생성자 호출
+            Console.WriteLine("두 번째 위치");
+            Sample sample = new Sample();
+            Console.WriteLine("세 번째 위치");
+
+
         }
 
         // 오버로딩 주의점
