1ba9c93d934cdab8b1b2c0760acbbbfc791444af
subin1848 <s2315@e-mirim.hs.kr>
2025-06-02 14:06:23 +0900

    Sample 소멸자 정의

--- Sample.cs
+++ Sample.cs
@@ -14,5 +14,9 @@ namespace PropExample
             value = 10;
             Console.WriteLine("정적 생성자 호출");
         }
+        ~Sample()   // Sample 소멸자 정의 - 객체가 소멸될 때 호출(GC 마음 or 프로그램 종료 시)
+        {
+            Console.WriteLine("Sample 객체 소멸자 호출");
+        }
     }
 }
